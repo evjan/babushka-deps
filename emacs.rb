3197af6e240839069d933f8a49de752fa2797c72
Peter Evjan <hello@peterevjan.com>
2016-07-22 14:32:25 +0200

    Don't use dots, they are for babushka templates

--- emacs.rb
+++ emacs.rb
@@ -1,16 +1,16 @@
-dep 'spacemacs.config' do
+dep 'spacemacs config' do
   met? { "~/.emacs.d".p.exists? }
   meet { shell "git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d" }
 end
 
-dep 'spacemacs.dotfile' do
+dep 'spacemacs dotfile' do
   met? { shell "fgrep 'dotspacemacs-line-numbers t' ~/.spacemacs" }
   meet { shell "curl https://raw.githubusercontent.com/evjan/dotfiles/master/.spacemacs > ~/.spacemacs" }
 end
 
 dep 'emacs' do
-  requires 'spacemacs.config'
-  requires 'spacemacs.dotfile'
+  requires 'spacemacs config'
+  requires 'spacemacs dotfile'
   met? { which 'emacs' }
   meet { installs 'emacs' }
 end
