33d5bc7b55fc17ec65127fb24c6f587ada09a760
Peter Evjan <hello@peterevjan.com>
2016-07-22 20:33:55 +0200

    Adding .agignore

--- ag.rb
+++ ag.rb
@@ -1,3 +1,12 @@
+dep 'ag' do
+  requires 'ag.bin', 'ag config'
+end
+
 dep 'ag.bin' do
   installs 'silversearcher-ag'
+end
+
+dep 'ag config' do
+  met? { '~/.agignore'.p.exists? }
+  meet { shell 'echo "logs" > ~/.agignore' }
 end
